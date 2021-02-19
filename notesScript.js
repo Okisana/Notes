177b1fbec4aea41b3adf171529dcd90ab897ce06
Andris Silis <andris.silis@gmail.com>
2021-02-19 13:53:57 +0200

    Code style

--- notesScript.js
+++ notesScript.js
@@ -1,107 +1,84 @@
 "use strict";
 
-window.addEventListener('DOMContentLoaded', ()=>{
+const onLoad = () => {
+  document.getElementById("rightSection").value = ""; // empty textarea
 
-document.getElementById("rightSection").value = ""; // empty textarea
+  const notes = [];
 
-let newNoteButton = document.getElementById('newNoteButton'); // poga pievieno jaunas piezīmes nosaukumu
-let noteName; // jaunas piezimes nosaukums
-let noteText = document.getElementById("rightSection"); // textarea
-const textareaValue = ()=>document.getElementById("rightSection").value;  // textarea saturs FUNKCIJA!!!!
-      
-let noteNameButton = document.querySelectorAll("button.noteNameButton");
-const liElement = document.querySelectorAll('.liBorders');
-const saveButton = document.querySelector('#buttonSave');
-const newnamesList = document.querySelector('.ulList');
-const deleteButton = document.getElementById('buttonDelete');
-/* note Structure
-const note1 = {
-  id:
-  name: "first Note",
-  content: "hello"
-};
-*/
-const notes = [];
+  const findNoteById = (id) => notes.find((note) => note.id === id);
 
-function findNoteById(id){
-  return notes.find((note) => note.id === id);
+  const newNoteButton = document.getElementById('newNoteButton'); // poga pievieno jaunas piezīmes nosaukumu
+  // jaunas piezīmes izveidošana
+  newNoteButton.onclick = () => {
+    const noteName = prompt('Enter note name: ', '');
+
+    document.getElementById("rightSection").value = "";
+
+    if (noteName) {
+      /* Structure of a note
+        {
+          id:
+          name: "first Note",
+          content: "hello"
+        }
+      */
+      const newnote = {
+        name: noteName,
+        id: notes.length+1,
+      };
+
+      notes.push(newnote);
+
+      const newnamesList = document.querySelector('.ulList');
+      newnamesList.innerHTML += `
+        <li class="liBorders">
+          <button class="noteNameButton" data-id="${newnote.id}"> ${newnote.name} </button>
+        </li>
+      `;
+    }
+  };
+
+  const saveButton = document.querySelector('#buttonSave');
+  saveButton.addEventListener('click', () => {
+    const textareaValue = () => document.getElementById("rightSection").value;  // textarea saturs FUNKCIJA!!!!
+
+    const newContent = textareaValue();
+
+    const latestNote = notes[notes.length -1]; // masīva pēdējais elements
+      latestNote.content = newContent;
+      console.log(notes);
+    });
+
+    console.log("notes", notes); // textarea saturs ielikts in array
+
+    // event delegation from parent ulList to child classes noteNameButton
+    document.querySelector('.ulList').addEventListener('click', (event) => {
+      const noteText = document.getElementById("rightSection"); // textarea
+
+      const id = parseInt(event.target.getAttribute('data-id'));
+      const note = findNoteById(id);
+      noteText.value = note.content ;
+
+      console.log(event);
+    /*
+      if(event.target && event.target.matches("li.liBorders") ){
+        event.target.classList.add('activeNoteName'); // Kā noņemt no elementa class activeNoteName,
+        // kad ir uzklikšķināta cita poga?
+        console.log(event.target.classList.contains('activeNoteName'));
+            }
+          */
+    });
+
+    // izdzēst Note ar pogu delete
+    const deleteButton = document.getElementById('buttonDelete');
+    deleteButton.addEventListener('click', () => {
+    });
+
+    document.querySelector('.ulList').addEventListener('click', (event) => {
+      if (event.target && event.target.contains('activeNoteName')) {
+        event.target.remove();
+      }
+    });
 }
 
-//newnamesList.innerHTML = ""; // iztukšot html elementus
-
-//jaunas piezīmes izveidošana
-newNoteButton.onclick = function(){
-  noteName = prompt('Enter note name: ', '');
-
-  document.getElementById("rightSection").value = "";
-
-  if(noteName){
-       let newnote = {
-      name: noteName,
-        id: notes.length+1, 
-        };
-
-    notes.push(newnote);
-
-    newnamesList.innerHTML += ` 
-    <li class="liBorders">
-      <button class="noteNameButton" data-id="${newnote.id}"> ${newnote.name} </button>
-      </li>`;
-
-        } 
-
-     };
-
-     //
-saveButton.addEventListener('click', () =>{
-  
-  const newContent = textareaValue();
-  // .content.push(document.getElementById("rightSection").value);  
- const latestNote = notes[notes.length -1]; // masīva pēdējais elements 
-  latestNote.content = newContent;
-   console.log(notes);
-});
-
-console.log("notes", notes); // textarea saturs ielikts in array
-
-// event delegation from parent ulList to child classes noteNameButton
-document.querySelector('.ulList').addEventListener('click', (event)=>{
-  let id = parseInt(event.target.getAttribute('data-id'));
-  const note = findNoteById(id);
-  noteText.value = note.content ;
-
-  console.log(event);
-/*
-  if(event.target && event.target.matches("li.liBorders") ){
-    event.target.classList.add('activeNoteName'); // Kā noņemt no elementa class activeNoteName,
-    // kad ir uzklikšķināta cita poga?
-    console.log(event.target.classList.contains('activeNoteName'));
-        }
-       */
-   });
-
-   // izdzēst Note ar pogu delete
-   deleteButton.addEventListener('click',()=>{
-      document.querySelector('.ulList').addEventListener('click', (event)=>{
-        if(event.target && event.target.contains('activeNoteName')){
-          event.target.remove();
-       }
-     });
-   });
-
-  
-
- 
-  
-  
-
-
-
-});
-  
-
-
-
-
-  
-
+window.addEventListener('DOMContentLoaded', onLoad);
